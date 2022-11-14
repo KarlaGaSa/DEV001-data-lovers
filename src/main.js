495f2902a77c0f785820b00a165a7f707a9f1eb5
Silvana Gonzalez <algomio2000@gmail.com>
2022-11-14 12:49:37 -0500

    probando modal

--- src/main.js
+++ src/main.js
@@ -2,92 +2,82 @@
 import data from './data/pokemon/pokemon.js';
 import { searchFilter, ordenAZPokemon, ordenZAPokemon } from './data.js';
 
+
+
 // ---- Mostrar cartas de cada Poke ----
 const infoPokemon = data.pokemon;
 const cardInfo = document.getElementById("cont-card");
+cardInfo.classList.add("cont-card")
 
 infoPokemon.forEach((persona) => {
     cardInfo.innerHTML += cardTemplate(persona.img, persona.num, persona.name)
 });
 
-function cardTemplate(img, num, name) {
+function cardTemplate(img, num, name,myBtn) {
+    
     const pokemonHTML = `
   <div class="cont.card" id=${num}>
   <div class="card" id="card">
   <img class="img" src=${img} </img><div> 
   <p><class= "namepokemon">${name} </p>
   <p><class= "numpokemon">${num}</p><br>
-  <button class="botonVerMas" id="myBtn">Ver más</button>
+  
   </div>
   </div>
+  <button class="myBtn" id=${myBtn}>modal</button>
   </div>
   `;
+  /*cont.card.addEventListener('click', () => {
+    const show = verMas(pokem);
+    show.classList.add('modal');
+  });*/
     return pokemonHTML
 }
+
+
+
 // --- Botón regresar a Home
 
 const home = document.getElementById("home");
-home.addEventListener("click",() =>{
-    window.location.reload();  
+home.addEventListener("click", () => {
+    window.location.reload();
 });
 
 /*const modal = document.getElementById("myModal");
-const btn = document.getElementById("myBtn");
+//const btn = document.getElementById("myBoton")
 const span = document.getElementsByClassName("close")[0];
- btn.addEventListener("click",()=>{
-    const verMas= (pokemon)=> {
-    const detallepokemonHTML= `
-    <div class= "general">
-      <img src="${pokemon.img}">
-   </div>
-   <div class="cardTipos-title">
-      <p>${pokemon.name}  ${pokemon.num}</p>
-   </div>
-   <div class="tipo">
-     <p> Tipo: ${pokemon.type}.</p>
-     <p> Altura: ${pokemon.size.height} Peso: ${pokemon.size.weight}</p>
-   </div>
-   <div class ="resistencia">
-      <p>Fortalezas: ${pokemon.resistant}</p>
-   </div>
-   <div class = "debilidades">
-      <p> Debilidades: ${pokemon.weaknesses}</p>
-   </div>
-   <div class ="about">
-      <p>Descripción:<br> ${pokemon.about}</p>
-   </div>
-   </div>
-   `
-   return detallepokemonHTML;
-};
-    
+
+
+
+const btn = document.getElementById("myBoton");
+btn.addEventListener("click", () => {
+    verMas();
 });*/
+
 
 // --- Boton Scroll ---
 const btn_scrolltop = document.getElementById("btn_scrolltop")
 btn_scrolltop.addEventListener('click', () => {
-  window.scrollTo(0, 0)
+    window.scrollTo(0, 0)
 })
 
 window.onscroll = () => {
-add_btn_scrolltop()
+    add_btn_scrolltop()
 }
 
 const add_btn_scrolltop = () => {
-if (window.scrollY < 300) {
-  btn_scrolltop.classList.remove("btn-scrolltop-on")
-} else {
-  btn_scrolltop.classList.add("btn-scrolltop-on")
-}
+    if (window.scrollY < 300) {
+        btn_scrolltop.classList.remove("btn-scrolltop-on")
+    } else {
+        btn_scrolltop.classList.add("btn-scrolltop-on")
+    }
 }
 
 // ---- Buscar por nombre
 const search = document.getElementById("search");
-const prueba = document.getElementById("botonBuscar")
-prueba.addEventListener("click", () => {
+search.addEventListener("keyup", () => {
     const text = search.value;
     let busca = searchFilter(infoPokemon, text)
-    /*  console.log(busca) */
     cardInfo.innerHTML = " "
     busca.forEach((nombre) => {
 
@@ -95,28 +85,70 @@ prueba.addEventListener("click", () => {
     });
 })
 
+
 //Ordenar de A a la Z
 const ordenAZ = document.getElementById("A-Z");
 //const ordenZa = document.getElementById("Z-A");
-ordenAZ.addEventListener("click" , () => {
-    const orden1= ordenAZ;
-    let ordenada = ordenAZPokemon(infoPokemon,orden1) 
+ordenAZ.addEventListener("click", () => {
+    const orden1 = ordenAZ;
+    let ordenada = ordenAZPokemon(infoPokemon, orden1)
     //console.log(ordenada);
     cardInfo.innerHTML = " "
-   ordenada.forEach((nombre) => {
-       cardInfo.innerHTML += cardTemplate(nombre.img, nombre.num, nombre.name)
+    ordenada.forEach((nombre) => {
+        cardInfo.innerHTML += cardTemplate(nombre.img, nombre.num, nombre.name)
     });
 })
 
 //Ordenar de Z a la A
 const ordenZA = document.getElementById("Z-A");
-ordenZA.addEventListener("click" , () => {
-    const orden2= ordenZA;
-    let desordenada = ordenZAPokemon(infoPokemon,orden2) 
+ordenZA.addEventListener("click", () => {
+    const orden2 = ordenZA;
+    let desordenada = ordenZAPokemon(infoPokemon, orden2)
     //console.log(ordenada);
     cardInfo.innerHTML = " "
-   desordenada.forEach((nombre) => {
-       cardInfo.innerHTML += cardTemplate(nombre.img, nombre.num, nombre.name)
+    desordenada.forEach((nombre) => {
+        cardInfo.innerHTML += cardTemplate(nombre.img, nombre.num, nombre.name)
     });
 })
+
+let modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+
+infoPokemon.forEach((persona) => {
+    modal.innerHTML += verMas(persona.img, persona.num, persona.name, persona.type)
+
+function verMas(img, name, num, type){
+   
+
+    const detallepokemonHTML = `
+  <div class="cont.card" id=${num}>
+  <div class="card" id="card">
+  <img class="img" src=${img} </img><div> 
+  <p><class= "namepokemon">${name} </p>
+  <p><class= "numpokemon">${num}</p><br>
+  <p><class""pokemontipo">${type}</p><br>
+  </div>
+  </div>
+  </div>
+  `;
+
+return detallepokemonHTML;
+}
+});
+
+cardinfo.click = function() {
+    modal.style.display = "block";
+  }
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+
 
