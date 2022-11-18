2d1a5dad812697d665615b8590cb52d4ae62a663
Karla GaSa <karlagsanabria@gmail.com>
2022-11-18 10:07:03 -0600

    Updates

--- data.js
+++ data.js
@@ -1,20 +1,41 @@
-// estas funciones son de ejemplo
-/*export const pokemon = () => {
-  return 'pokemon';
+
+//import data from './data/pokemon/pokemon.js'
+
+export const searchFilter =  (pokemon,nombrePokemon)=> {
+  const nameFilter= pokemon.filter (x=>
+    x.name.startsWith(nombrePokemon.toLowerCase()))
+    return nameFilter
+  };
+ 
+
+  export const ordenAZPokemon=(array)=>{
+
+  return array.sort(function (a, b) {
+    
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name> b.name) {
+      return 1;
+  }
+})
 };
-export const anotherExample = () => {
-  return 'OMG';
-};*/
 
-//import pokemon from './data/pokemon/pokemon.js';
-//const pokemon = 
+export const ordenZAPokemon=(array)=>{
 
-/*Te recomendamos que este archivo contenga toda la funcionalidad que corresponda a obtener, procesar y manipular datos (tus funciones). Por ejemplo:
-
-filterData(data, condition): esta función filter o filtrar recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
-
-sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro, data, nos entrega los datos. El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
-
-Estos nombres de funciones y de parámetros son solamente referenciales, lo que decidas depende de tu propia implementación.
-
-Estas funciones deben ser puras e independientes del DOM. Estas funciones serán después usadas desde el archivo src/main.js, al cargar la página, y cada vez que el usuario interactúe (click, filtrado, ordenado, ...).*/
+  return array.sort(function (a, b) {
+    
+    if (a.name < b.name) {
+      return 1;
+    }
+    if (a.name > b.name) {
+      return -1;
+    }
+  })
+}
+  
+export const filtroPorTipo =  (pokemon, ptype)=> {
+  const busquedaXTipo= pokemon.filter (x => 
+  x.type.includes(ptype));
+  return busquedaXTipo;
+  };
--- main.js
+++ main.js
@@ -1,28 +1,112 @@
 
 import data from './data/pokemon/pokemon.js';
+import { searchFilter, ordenAZPokemon, ordenZAPokemon,filtroPorTipo  } from './data.js';
 
-//AQUÍ YA LOGRÉ QUE SALGA EL ÚLTIMO POKEMON, IMAGEN, NOMBRE Y NUM usando: import data from './data/pokemon/pokemon.js';
-const cardInfo = document.getElementById("cont-card");
-const elementPokemon = document.createElement("div");
-elementPokemon.classList.add("solucion")
+// ---- Constantes declaradas ----
 const infoPokemon = data.pokemon;
+const cardInfo = document.getElementById("cont-card");
 
-//TODO: creare funcion que reciba un arreglo de objetos(pokemones) y retorna un arreglo de cards (etiquetas HTML)
+// --- Mostrando tarjetas ---
+infoPokemon.forEach((persona) => {
+  cardInfo.innerHTML += cardTemplate(persona.img, persona.num, persona.name, persona.type)
 
-  infoPokemon.forEach((card) => {
-    const pokemonHTML = `
-    <div class="cont.card" id=${card.num}>
-    <img class="detalle" id="circulo"></img>
-    <div class="card" id="card"><img class="img" src=${card.img} </img>
-    <p><class= "namepokemon">${card.name} </p>
-    <p><class= "numpokemon">${card.num}</p>
-    </div>
-    </div>
-    `;
+});
 
-    elementPokemon.innerHTML += pokemonHTML
-    cardInfo.appendChild(elementPokemon)
-    //console.log(pokemonParaHTML)
+function cardTemplate(img, num, name, type) {
+
+  const pokemonHTML = `
+  <div class="cont.card" id=${num}>
+  <div class="card" id="card">
+  <img class="img" src=${img}>
+  <p>${name} </p>
+  <p>${num}</p><br>
+  <p>${type}</p><br>
+  </div>
+  </div>
+  `;
+
+  return pokemonHTML
+}
+
+// --- Botón regresar a Home
+
+const home = document.getElementById("home");
+home.addEventListener("click", () => {
+  window.location.reload();
+});
+
+// --- Boton Scroll ---
+const btn_scrolltop = document.getElementById("btn_scrolltop")
+btn_scrolltop.addEventListener('click', () => {
+  window.scrollTo(0, 0)
+})
+
+window.onscroll = () => {
+  add_btn_scrolltop()
+}
+
+const add_btn_scrolltop = () => {
+  if (window.scrollY < 300) {
+    btn_scrolltop.classList.remove("btn-scrolltop-on")
+  } else {
+    btn_scrolltop.classList.add("btn-scrolltop-on")
   }
-  );
+}
 
+// ---- Buscar por nombre
+const search = document.getElementById("search");
+search.addEventListener("keyup", () => {
+  const text = search.value;
+  let busca = searchFilter(infoPokemon, text)
+  cardInfo.innerHTML = " "
+  busca.forEach((nombre) => {
+
+    cardInfo.innerHTML += cardTemplate(nombre.img, nombre.num, nombre.name, nombre.type)
+  });
+})
+
+/* --- Buscar por TIPO --- */
+const filtroTipo = document.getElementById('filtroTipo');
+filtroTipo.addEventListener('change', () => {
+  if (filtroTipo.value === 'all') {
+    //console.log(cardInfo)
+    cardInfo.innerHTML = " "
+  infoPokemon.forEach((nombre) => {
+
+    cardInfo.innerHTML += cardTemplate(nombre.img, nombre.num, nombre.name, nombre.type)
+  });
+  } else {
+    const catchFilter = filtroPorTipo(infoPokemon, filtroTipo.value);
+    //console.log(catchFilter);
+    cardInfo.innerHTML = "";
+  catchFilter.forEach((nombre) => {
+
+    cardInfo.innerHTML += cardTemplate(nombre.img, nombre.num, nombre.name, nombre.type)
+  });
+  }
+});
+
+//Ordenar de A a la Z
+const ordenAZ = document.getElementById("A-Z");
+//const ordenZa = document.getElementById("Z-A");
+ordenAZ.addEventListener("click", () => {
+  const orden1 = ordenAZ;
+  let ordenada = ordenAZPokemon(infoPokemon, orden1)
+  //console.log(ordenada);
+  cardInfo.innerHTML = " "
+  ordenada.forEach((nombre) => {
+    cardInfo.innerHTML += cardTemplate(nombre.img, nombre.num, nombre.name, nombre.type)
+  });
+})
+
+//Ordenar de Z a la A
+const ordenZA = document.getElementById("Z-A");
+ordenZA.addEventListener("click", () => {
+  const orden2 = ordenZA;
+  let desordenada = ordenZAPokemon(infoPokemon, orden2)
+  //console.log(ordenada);
+  cardInfo.innerHTML = " "
+  desordenada.forEach((nombre) => {
+    cardInfo.innerHTML += cardTemplate(nombre.img, nombre.num, nombre.name, nombre.type)
+  });
+})
